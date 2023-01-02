3a63875e6e046c3736c7ed6e9ee291e4023d35af
SeungMin Kang <andrewk93@fnguide.com>
2023-01-02 13:03:24 +0900

    - 할 일 추가 form 추가

--- src/App.js
+++ src/App.js
@@ -42,6 +42,25 @@ export default class App extends Component {
     this.setState({todoData: newTodoData});
   }
 
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    // 페이지 리로드 방지
+    e.preventDefault();
+    
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [...this.state.todoData, newTodo ]});
+  }
+
   render() {
     return (
       <div className='container'>
@@ -57,6 +76,23 @@ export default class App extends Component {
             <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
           </div>
           ))}
+
+          <form style={{ display: 'flex' }} onSubmit={this.handleSubmit}>
+            <input
+              type='text'
+              name='value'
+              style={{ flex: '10', padding: '5px' }}
+              placeholder='해야 할 일을 입력하세요.'
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type='submit'
+              value='입력'
+              className='btn'
+              style={{ flex: '1' }}
+            />
+          </form>
 
         </div>
       </div>
