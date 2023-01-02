2c82c0cf43bfa000452732b604ddb5f40f5c4d88
SeungMin Kang <andrewk93@fnguide.com>
2023-01-02 17:23:39 +0900

    - Drag and Drop 기능 추가.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import List from './components/List';
 
 export default function App() {
   
-  const [todoData, setTodoData] = useState([]);
+  const [todoData, setTodoData] = useState([
+    {
+      id: '1',
+      title: '공부하기',
+      completed: true,
+    },
+    {
+      id: '2',
+      title: '청소하기',
+      completed: false,
+    },
+  ]);
   const [value, setValue] = useState('');  
 
   const handleSubmit = (e) => {
@@ -26,15 +37,16 @@ export default function App() {
   }
 
   return (
-    <div className='container'>
-      <div className='todoBlock'>
-        <div className='title'>
+    <div className='flex items-center justify-center w-screen h-screen bg-blue-100'>
+      <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
+        <div className='flex justify-between mb-3'>
           <h1>할 일 목록</h1>
         </div>
 
         <List todoData={todoData} setTodoData={setTodoData} />
 
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+
       </div>
     </div>
   );
